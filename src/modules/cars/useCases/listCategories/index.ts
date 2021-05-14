dd10b88086c438c809d970a002a37f42eac4fe0a
Higor Miguel <higor.mig@gmail.com>
2021-05-14 14:15:50 -0300

    Refectoring specification

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,8 +1,8 @@
-import { CategoryRepository } from "../../repositories/CategoryRepository";
+import { CategoryRepository } from "../../repositories/implementations/CategoryRepository";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-const categoriesRepository = new CategoryRepository();
+const categoriesRepository = CategoryRepository.getInstance();
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 const listCategoriesController = new ListCategoriesController(
     listCategoriesUseCase
